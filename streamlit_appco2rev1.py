c9a0970c6dac0b8228b717c8ae80e7e4211f1332
NScreator19 <contact.natashays@gmail.com>
2025-07-01 15:10:14 +0700

    Update streamlit_appco2rev1.py

--- streamlit_appco2rev1.py
+++ streamlit_appco2rev1.py
@@ -132,9 +132,11 @@ stec = st.number_input("STEC (MJ/ton Clinker)", value=3340.0)
 tsr = st.number_input("TSR  (%)", value=13.0)
 fuel_ef = st.number_input("fuel_ef  (kg CO2/MJ)", value=0.0958)
 calcination_factor = st.number_input("Calcination Factor (kg CO2/kg Clinker)", value=0.531)
+factor = st.number_input("factor adj", value=1.01)
 
 co2_process = clinker_factor/100  * calcination_factor * 1000
 co2_fuel = stec * fuel_ef * (1 - tsr / 100) * clinker_factor/100
-co2_total = co2_process + co2_fuel
+co2_tot = co2_process + co2_fuel
+co2_total=co2_tot * factor 
 
 st.metric("CO2 Specific Net (kg CO2/ton cement Eq)", f"{co2_total:.0f}")
